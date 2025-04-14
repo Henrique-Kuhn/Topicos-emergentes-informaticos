660e2048c0668c232a3dde2adae0408ee0316123
Henrique-Kuhn <henrikabreu@hotmail.com>
2025-04-14 14:34:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function saudacao(){
+    alert("Texto trocado");
+    document.getElementById("texto").innerHTML = "texto trocado"
+}
+function resetar(){
+    document.body.style.backgroundColor='white';
+    document.getElementById("texto").innerHTML ="texto resetado"
+    document.body.style.color ="black";
+}
+function preto(){
+       document.body.style.backgroundColor='black';
+       document.body.style.color ="white";
+}
+// function branco(){
+//     document.body.style.backgroundColor='white';
+// }
+function verde(){
+    document.body.style.backgroundColor='green';
+}
+function azul(){
+    document.body.style.backgroundColor='blue';
+}
